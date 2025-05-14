70d814c86f73de2eb49145d64f81bc799e0a14cf
Fianchetto1234 <oscarsalek@gmail.com>
2025-05-14 15:17:35 +1000

    add api retrieval

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request jsonify
 import pickle
 
 tokenizer = pickle.load(open('models/cv.pkl','rb'))
@@ -16,6 +16,14 @@ def submit():
     tokenised_email = tokenizer.transform([email_text]) 
     prediction = model.predict(tokenised_email)
     return render_template('index.html', response=prediction[0], website_text=email_text)
+
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    data = request.get_json(force=True)
+    email_text = data['email_body']
+    tokenised_email = tokenizer.transform([email_text])
+    prediction = model.predict(tokenised_email)
+    return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
